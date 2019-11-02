605449ceec20848fac3d410de11062e198aae540
AlekseyKireychik <kireychika@mail.ru>
2019-11-02 14:05:10 +0300

    add instructor Page

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,7 +1,31 @@
-// accordion FAQ page
-$(".faq__btn").on("click", function() {
-  $(".panel").removeClass("is-active");
-  $(this)
-    .parents(".panel")
-    .toggleClass("is-active");
+$(document).ready(function() {
+  // accordion FAQ page
+  $(".faq__btn").on("click", function() {
+    $(".panel").removeClass("is-active");
+    $(this)
+      .parents(".panel")
+      .toggleClass("is-active");
+  });
+  //more-less
+  // $('.nav-toggle').click(function () {
+  //   var collapse_content_selector = $(this).attr('href');
+  //   var toggle_switch = $(this);
+  //   $(collapse_content_selector).toggle(function () {
+  //       if ($(this).css('display') == 'none') {
+  //           toggle_switch.html('Read More');
+  //       } else {
+  //           toggle_switch.html('Read Less');
+  //       }
+  //   });
+  // });
+
+  //slider
+  // $(".slider").slick({
+  //   dots: false,
+  //   arrows: false,
+  //   speed: 300,
+  //   slidesToShow: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 4000
+  // });
 });
