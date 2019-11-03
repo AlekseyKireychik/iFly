34ca0de9472ef92d279b3ddb8e87868340ae1a5b
AlekseyKireychik <kireychika@mail.ru>
2019-11-03 18:02:53 +0300

    add new vers header

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,30 +1,53 @@
 $(document).ready(function() {
+  //nav-menu
+  $(".burger-menu").on("click", function(event) {
+    event.preventDefault();
+    $("#nav-menu").toggleClass("is-active");
+    $(".burger-menu").toggleClass("is-active");
+    $("#nav-menu").on('mouseenter', function() {
+      $(this).addClass("is-active");
+    });
+    $("#nav-menu").on('mouseleave', function() {
+      $(this).removeClass("is-active");
+      $(".burger-menu").removeClass("is-active");
+    });
+  });
+
+  $(".header__link").hover (function(event) {
+    event.preventDefault();
+    $(this).siblings(".header__subList").toggleClass("is-active");
+    $(".header__subList").on('mouseenter', function() {
+      $(this).addClass("is-active");
+    });
+    $(".header__subList").on('mouseleave', function() {
+      $(this).removeClass("is-active");
+    });
+  });
+
   // accordion FAQ page
   $(".faq__btn").on("click", function() {
     $(".panel").removeClass("is-active");
-    $(this)
-      .parents(".panel")
-      .toggleClass("is-active");
+    $(this).parents(".panel").toggleClass("is-active");
   });
 
-  $.extend($.validator.messages, {
-    required: "Error"
-  });
+  // $.extend($.validator.messages, {
+  //   required: "Error"
+  // });
 
-  $("#form").validate({
-    rules: {
-      name: {
-        required: true
-      },
-      fname: {
-        required: true,
-        fname: true
-      },
-      tel: {
-        required: true
-      }
-    }
-  });
+  // $("#form").validate({
+  //   rules: {
+  //     name: {
+  //       required: true
+  //     },
+  //     fname: {
+  //       required: true,
+  //       fname: true
+  //     },
+  //     tel: {
+  //       required: true
+  //     }
+  //   }
+  // });
 
   //more-less
   // $('.nav-toggle').click(function () {
